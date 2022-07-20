82cbe995e621f2fc5f29589e2fb1fa47c063a8c1
seepika <54041441+seepika@users.noreply.github.com>
2022-07-20 17:07:16 +0530

    Developed By Seepika

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: , lng: },
+          zoom: 8
+        });
+      }
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 300
+      },
+      800
+    );
+  }
+});
+
+
